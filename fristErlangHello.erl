0fd00c76e23791ff7f661b584ab888d6a9517eec
soplist <sopcase@yahoo.com>
2016-05-26 17:06:49 +0800

    erlang.20160526

---
+++ fristErlangHello.erl
@@ -0,0 +1,23 @@
+%% Author: wangkang
+-module(fristErlangHello).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Function
+%%
+
+-export([helloWorld/0]).
+-export([]).
+
+%%
+%% API Functions
+%%
+
+%%
+%% Local Functions
+%%
+
+helloWorld() -> io:format("erlang:hello world~n").
